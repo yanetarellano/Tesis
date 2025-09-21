df2431a1e453817100e3d53e83f0db5f536c2de9
yanetarellano <yanet.arellano@gmail.com>
2025-09-21 10:42:01 -0600

    Update AnalisisModelo.R

--- AnalisisModelo.R
+++ AnalisisModelo.R
@@ -1,10 +1,19 @@
-#install.packages("readxl")
+# Autora: Yanet Karina González Arellano
+# Script(guión) para el modelo de regresión lineal múltiple
+
+# Instalar paquetes necesarios
+install.packages("readxl","lmtest","car","leaps")
+
+# Cargar los paquetes necesarios 
 library(readxl)
 library(lmtest)
 library(car)
 library(leaps)
-libro <- read_excel("/home/juanlps/Dropbox/YanetYJlps/YanetProject/Tesis/AnalisisEstadistico3.xlsx", sheet = "Notas")
-# Crea dataset limpio con nombres simples
+
+# Cargar el archivo en excel con las calificaciones finales 
+libro <- read_excel("/Tesis/AnalisisEstadistico.xlsx", sheet = "Notas")
+
+# Crear dataset limpio con nombres simples
 datos <- data.frame(
   y  = libro$final,
   x1 = as.factor(libro$tratamiento), 
@@ -13,33 +22,31 @@ datos <- data.frame(
   #x4 = libro$proyecto,           
   #x5 = libro$quizes,             
   x6 = libro$tareas            
-  #x7 = libro$diagnostico          
+  #x7 = libro$diagnostico,          
   #x8 = libro$parciales           
 )
+
+# Convertir calificación final a numérica
 datos$y <- as.numeric(as.character(datos$y))
+
+# Llamar el comando lm (linear model) para el modelo lineal, análisis y resumen 
 modelo <- lm(y~x1+x6,data=datos)
 summary(modelo)
+
+# Prueba de Normalidad
+shapiro.test(residuals(modelo))
+
+# Prueba de Homocedasticidad
+leveneTest(y ~ x1, data = datos)
   
- 
-  # Normalidad
-  shapiro.test(residuals(modelo))
-  #lillie.test(residuals(modelo))
+# Prueba de Independencia
+durbinWatsonTest(modelo)
   
-  # Homogeneidad de varianzas
-  leveneTest(y ~ x1, data = datos)
-  
-  
-  
-  # Independencia de errores
-  durbinWatsonTest(modelo)
-  
-  # Multicolinealidad
-  vif(modelo)
-  
-  # Para anlizar variables relevantes
-  # regfit <- regsubsets(final ~ tratamiento + asesorias + asistencias +
-  #                      proyecto + quizes + tareas + diagnostico
-  #                      + parciales, data = datos, nvmax = 8)
+# Prueba de Multicolinealidad
+vif(modelo)
+
+# Para analizar variables relevantes
+  # regfit <- regsubsets(y~x1+x2+x3+x4+x5+x6+x7+x8 , data = datos, nvmax = 8)
   #  
   # summary(regfit)
   # res <- summary(regfit)
@@ -47,14 +54,3 @@ summary(modelo)
   # res$adjr2   # R² ajustado de cada modelo
   # res$cp      # Cp de Mallows
   # res$bic     # BIC de cada modelo
-  #  
-  # # Método 1: Usando la función pcr() del paquete pls (recomendado)
-  # modelo_pcr <- pcr(final ~ tratamiento + asesorias + asistencias + 
-  #                     proyecto + quizes + tareas + diagnostico + parciales, 
-  #                   data = datos, 
-  #                   scale = TRUE,  # Estandarizar predictores
-  #                   validation = "CV")  # Validación cruzada
-  # 
-  # # Resumen del modelo
-  # summary(modelo_pcr)
-  # 
